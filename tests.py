faa1d1227421eedd4864e1b865d7aba014187170
zadenis2007-bit <zadenis2007@gmail.com>
2026-06-03 22:07:54 +0300

    tests.py

--- tests.py
+++ tests.py
@@ -1,22 +1,11 @@
-import os
 import unittest
-from file_table import FileTable
+from database import Table
 
 
-class TestFileTable(unittest.TestCase):
-
-  TEST_FILE = "test_data.json"
+class TestTable(unittest.TestCase):
 
   def setUp(self):
-    self.table = FileTable(self.TEST_FILE)
-
-    self.table.data = []
-    self.table.next_id = 1
-    self.table.save()
-
-  def tearDown(self):
-    if os.path.exists(self.TEST_FILE):
-      os.remove(self.TEST_FILE)
+    self.table = Table()
 
   def test_add(self):
     self.table.add({
@@ -24,9 +13,9 @@ class TestFileTable(unittest.TestCase):
       "age": 18
     })
 
-    self.assertEqual(len(self.table.data), 1)
-    self.assertEqual(self.table.data[0]["name"], "Denis")
-    self.assertEqual(self.table.data[0]["id"], 1)
+    self.assertEqual(len(self.table.get_all()), 1)
+    self.assertEqual(self.table.get_all()[0]["name"], "Denis")
+    self.assertEqual(self.table.get_all()[0]["id"], 1)
 
   def test_get_all(self):
     self.table.add({"name": "A", "age": 10})
@@ -75,20 +64,12 @@ class TestFileTable(unittest.TestCase):
     deleted = self.table.delete(1)
 
     self.assertTrue(deleted)
-    self.assertEqual(len(self.table.data), 0)
+    self.assertEqual(len(self.table.get_all()), 0)
 
   def test_delete_fail(self):
     deleted = self.table.delete(999)
 
     self.assertFalse(deleted)
-
-  def test_save_and_load(self):
-    self.table.add({"name": "Saved", "age": 30})
-
-    new_table = FileTable(self.TEST_FILE)
-
-    self.assertEqual(len(new_table.data), 1)
-    self.assertEqual(new_table.data[0]["name"], "Saved")
 
   def test_next_id_increment(self):
     self.table.add({"name": "A", "age": 1})
@@ -113,23 +94,18 @@ class TestFileTable(unittest.TestCase):
     self.assertEqual(result[0]["age"], 30)
 
   def test_empty_table(self):
-    result = self.table.get_all()
+    self.assertEqual(self.table.get_all(), [])
 
-    self.assertEqual(result, [])
+  def test_sort_empty_table(self):
+    self.assertEqual(self.table.sort_by("age"), [])
 
-  def test_load_invalid_json(self):
-    with open(self.TEST_FILE, "w", encoding="utf-8") as f:
-      f.write("INVALID JSON")
+  def test_sort_invalid_field(self):
+    self.table.add({"name": "Denis", "age": 18})
 
-    table = FileTable(self.TEST_FILE)
-
-    self.assertEqual(table.data, [])
-    self.assertEqual(table.next_id, 1)
-
-  def test_file_created(self):
-    self.table.add({"name": "File", "age": 1})
-
-    self.assertTrue(os.path.exists(self.TEST_FILE))
+    self.assertEqual(
+      self.table.sort_by("salary"),
+      []
+    )
 
 
 if __name__ == "__main__":
