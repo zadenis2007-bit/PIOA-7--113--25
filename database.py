7f137dccd18c7b32667f78977a5a237d01fea9f1
zadenis2007-bit <zadenis2007@gmail.com>
2026-06-03 21:57:46 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,46 +1,55 @@
-def add(self, record):
-    record["id"] = self.next_id
-    self.next_id += 1
-    self.data.append(record)
+class Table:
+    def __init__(self):
+        self.data = []
+        self.next_id = 1
+        
+    def add(self, record):
+        record["id"] = self.next_id
+        self.next_id += 1
+        self.data.append(record)
 
-def get_all(self):
-    return [record.copy() for record in self.data]
+    def get_all(self):
+        return [record.copy() for record in self.data]
 
-def filter(self, name):
-    result = []
+    def filter(self, name):
+        result = []
 
-    for record in self.data:
-        if record["name"] == name:
-            result.append(record)
+        for record in self.data:
+            if record["name"] == name:
+                result.append(record)
 
-    return result
+        return result
 
-def update(self, record_id, name, age):
-    for record in self.data:
-        if record["id"] == record_id:
-            record["name"] = name
-            record["age"] = age
-            return True
+    def update(self, record_id, name, age):
+        for record in self.data:
+            if record["id"] == record_id:
+                record["name"] = name
+                record["age"] = age
+                return True
 
-    return False
+        return False
 
-def delete(self, record_id):
-    for record in self.data:
-        if record["id"] == record_id:
-            self.data.remove(record)
-            return True
+    def delete(self, record_id):
+        for record in self.data:
+            if record["id"] == record_id:
+                self.data.remove(record)
+                return True
+    
+        return False
 
-    return False
+    def sort_by(self, field, reverse=False):
+        if not self.data:
+            return []
 
-def sort_by(self, field, reverse=False):
-    if not self.data:
-        return []
+        if field not in self.data[0]:
+            return []
 
-    if field not in self.data[0]:
-        return []
+        return sorted(
+            self.data,
+            key=lambda x: x[field],
+            reverse=reverse
+        )
 
-    return sorted(
-        self.data,
-        key=lambda x: x[field],
-        reverse=reverse
-    )
+class Database:
+    def __init__(self):
+        self.table = Table()
