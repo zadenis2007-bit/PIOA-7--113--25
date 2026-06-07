ee53978bf2d466f264f57afd278daa453bb17875
zadenis2007-bit <zadenis2007@gmail.com>
2026-06-07 23:11:52 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -4,19 +4,25 @@ from abc import ABC, abstractmethod
 class BaseTable(ABC):
 
     @abstractmethod
-    def add(self, record): pass
+    def add(self, record): 
+        pass
 
     @abstractmethod
-    def get_all(self): pass
+    def get_all(self): 
+        pass
 
     @abstractmethod
-    def filter(self, name): pass
+    def filter(self, **filters): 
+        pass
 
     @abstractmethod
-    def update(self, record_id, name, age): pass
+    def update(self, record_id, name, age): 
+        pass
 
     @abstractmethod
-    def delete(self, record_id): pass
+    def delete(self, record_id): 
+        pass
 
     @abstractmethod
-    def sort_by(self, field, reverse=False): pass
+    def sort_by(self, field, reverse=False): 
+        pass
