b7d4f5d076086900a2329c24205665d053906377
zadenis2007-bit <zadenis2007@gmail.com>
2026-06-08 14:01:45 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,13 @@ from file_table import FileTable
 
 class Database:
     def __init__(self, mode="memory"):
-        if mode == "file":
-            self.table = FileTable()
-        else:
+        if mode == "memory":
             self.table = MemoryTable()
+
+        elif mode == "file":
+            self.table = FileTable()
+
+        else:
+            raise ValueError(
+                f"Unknown database mode: {mode}"
+            )
